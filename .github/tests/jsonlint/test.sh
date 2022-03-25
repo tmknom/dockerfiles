5d0f4205b1a45d5dc011841f22505d19d32c7e41
tmknom <tmk.nom@gmail.com>
2022-03-25 10:54:27 +0900

    Create jsonlint test

---
+++ .github/tests/jsonlint/test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# test jsonlint
+
+TOOL_NAME=jsonlint
+
+CURRENT_DIR="$(
+  cd "$(dirname "$0")" || exit 1
+  pwd
+)"
+
+# shellcheck disable=SC1091
+source "${CURRENT_DIR}"/../xunit.sh
+
+run_test() {
+  docker run -it --rm -v "$(pwd):/work" -w /work \
+    --user 1111:1111 --read-only --security-opt no-new-privileges --cap-drop all \
+    ghcr.io/tmknom/dockerfiles/"${TOOL_NAME}":latest \
+    ".github/tests/${TOOL_NAME}/${test_case}_json.txt" \
+    >"${CURRENT_DIR}/${test_case}.log" 2>&1
+
+  echo "$?" >"${CURRENT_DIR}/${test_case}.exit_code.log"
+}
+
+run() {
+  local test_case
+  test_case="$1"
+  local expected_exit_code
+  expected_exit_code="$2"
+  local expected_result
+  expected_result="$3"
+
+  run_test "${test_case}"
+  verify_test "${test_case}" "${expected_exit_code}" "${expected_result}"
+}
+
+# test
+run "valid" "0" ""
+run "invalid" "1" "Error: Parse error on line 2"
+
+rm "${CURRENT_DIR}"/*.log
+echo "Success all test!"
